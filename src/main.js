de18782998022056c374922d7aea6477c1f88e1c
Karim Kompissi <karimkompissi@gmail.com>
2024-09-16 13:24:48 +0100

    edit files

--- src/main.js
+++ src/main.js
@@ -1,26 +1,34 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.join(__dirname, '/src/preload.js')
+        },
+    });
+
 
     win.loadFile('index.html')
+
+    // win.webContents.openDevTools();
 }
 
-app.whenReady().then(() => {
-    createWindow()
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
